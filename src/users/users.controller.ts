ea1e5ede507f132027871febbdcf7b5e52cfc095
Alexandra Juredieu <theuleaua@gmail.com>
2025-08-08 13:17:56 +0200

    feat(auth): add JWT authentication strategy and guards with user retrieval functionality

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth-guard';
+import { GetUser } from 'src/auth/get-user.decorator';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  getProfile(@GetUser() user: any) {
+    return user;
+  }
 
   @Post()
   create(@Body() dto: CreateUserDto) {
