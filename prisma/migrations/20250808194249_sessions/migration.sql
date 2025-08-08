735613b2627fa14beeeb128d439cc93ab3c5ac34
Alexandra Juredieu <theuleaua@gmail.com>
2025-08-08 21:45:06 +0200

    feat(session): add isDefault field to Session model

--- prisma/migrations/20250808194249_sessions/migration.sql
+++ prisma/migrations/20250808194249_sessions/migration.sql
@@ -1,8 +1,9 @@
 -- CreateTable
-CREATE TABLE `Session` (
+CREATE TABLE `Sessions` (
     `id` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NULL,
     `ownerId` VARCHAR(191) NOT NULL,
+    `isDefault` BOOLEAN NOT NULL DEFAULT false,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
@@ -10,4 +11,4 @@ CREATE TABLE `Session` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `Session` ADD CONSTRAINT `Session_ownerId_fkey` FOREIGN KEY (`ownerId`) REFERENCES `Users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `Sessions` ADD CONSTRAINT `Sessions_ownerId_fkey` FOREIGN KEY (`ownerId`) REFERENCES `Users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
